f0e7c480b3dd018bc0ea4ffa0a955b99a20c21fa
Kanittakp <kanitta.kp@kkumail.com>
2018-03-25 13:31:12 +0700

    kanitta

---
+++ test/detail.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+    <title>Detail</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+    <style>
+        body {
+            background-image: url("../pic/fire.jpg");
+            background-position: center;
+            font-family: Kanit;
+        }
+
+        #bgleft {
+            float: left;
+            width: 200px;
+        }
+
+        #bg {
+            background: white;
+            padding: 10px;
+            float: right;
+        }
+
+        table, td, tr, th {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <header>
+        <a href="index.php">
+            <img src="../pic/logo.png" width="150">
+        </a>
+    </header>
+    <div id="bgleft">
+        <br><br>
+        <img width="500" src="../pic/devil2.png">
+    </div>
+    <br><br>
+    <div id="bg">
+        <div align="center">
+            <span style="color:#7c2ca2;font-size: 60px;padding-right: 80px;">77  ํC</span>
+            <img src="../pic/weather/1.png" alt="ปลอดโปร่ง" width="150">
+            <span style="color:#f8001c;font-size: 60px;padding-left: 80px;">77  ํF</span>
+        </div>
+        <!-- <img src="../pic/weather/2.png" alt="ฝนตก" width="100">
+         <img src="../pic/weather/3.png" alt="เมฆมาก" width="120">
+         <img src="../pic/weather/4.png" alt="ร้อน" width="130">
+         <img src="../pic/weather/5.png" alt="ร้อนจัด" width="140">
+         <img src="../pic/weather/6.png" alt="หนาว" width="150">-->
+        <table class="table table-striped" style="width: 600px">
+            <thead>
+            <tr>
+                <th>Time</th>
+                <th>Temperature</th>
+                <th>Huminity</th>
+                <th>Pressure</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
